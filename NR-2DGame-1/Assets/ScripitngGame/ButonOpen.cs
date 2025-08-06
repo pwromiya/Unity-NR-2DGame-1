9467c617fa33afd259572415f16c3ddbdb8e5d83
kikaffq <qwertyuiopopqop@gmail.com>
2025-08-06 16:25:33 +0300

    prostotakinekak

---
+++ NR-2DGame-1/Assets/ScripitngGame/ButonOpen.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class ButonOpen : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [SerializeField] private GameObject buton;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("privet ");
+    }
+}
