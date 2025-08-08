37d227e4f8e9229d9dc3ec58e74d450457753cd1
kikaffq <qwertyuiopopqop@gmail.com>
2025-08-08 20:12:41 +0300

    update1.1
    + add double jump
    + add coment in code

--- NR-2DGame-1/Assets/ScriptingGame/ScriptHero/HeroControl.cs
+++ NR-2DGame-1/Assets/ScriptingGame/ScriptHero/HeroControl.cs
@@ -2,19 +2,21 @@ using UnityEngine;
 
 public class HeroControl : MonoBehaviour
 {
-    [Header("Hero Settings")]
+    [Header("Hero Settings")]                                                        // Hero settings variable ( speed moving + jump)
     [SerializeField] private float moveSpeed = 2.1f;
     [SerializeField] private float jumpForse = 3.0f;
+    [SerializeField] private float jumpThreshold = 0.5f;
 
-    [Header("Ground Check Settings")]
+    [Header("Ground Check Settings")]                                               // Ground Chek 
     public Transform groundCheck;
     private float GroundRadius = 0.3f;
     public LayerMask groundMask;
 
-    private Rigidbody2D rb;
+    private Rigidbody2D rb;                                                         // Inspektor Settings
     private SpriteRenderer spriteRender;
     private Animator animator;
     private bool isGroundet;
+    private bool doubleJump;
 
     private void Awake()
     {
@@ -30,30 +32,45 @@ public class HeroControl : MonoBehaviour
         HeroFlip();
     }
 
-    private void HeroMovent()
+    private void HeroMovent()                                                     // Movings 
     {
         float moveInput = Input.GetAxis("Horizontal");
         float SprintSpeed = Input.GetKey(KeyCode.LeftShift) ? 1.5f : 1.0f;
         rb.linearVelocity = new Vector2(moveInput * moveSpeed * SprintSpeed, rb.linearVelocity.y);
 
         //  ������������ ��������
-        bool isWalking = Mathf.Abs(moveInput) > 0.01f;
+        bool isWalking = Mathf.Abs(moveInput) > 0.01f;                            // Animators
         animator.SetBool("WalkBool", isWalking);
 
         Debug.Log($"�������� = {moveSpeed * SprintSpeed}");
     }
 
-    private void JumpHeroMovent()
+    private void JumpHeroMovent()                                                 // Jump
     {
         isGroundet = Physics2D.OverlapCircle(groundCheck.position, GroundRadius, groundMask);
 
-        if (Input.GetKeyDown(KeyCode.Space) && isGroundet)
+        if (isGroundet)
         {
-            rb.AddForce(Vector2.up * jumpForse, ForceMode2D.Impulse);
+            doubleJump = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))                                      //checking the keystroke
+        {                  
+
+            if (isGroundet)
+            {
+                rb.AddForce(Vector2.up * jumpForse, ForceMode2D.Impulse);
+            }
+            else if (!doubleJump && rb.linearVelocityY <= jumpThreshold)         //if the vertical speed is lower
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocityX, 0f);         //reset the vertical speed
+                rb.AddForce(Vector2.up * jumpForse, ForceMode2D.Impulse);
+                doubleJump = true;
+            }
         }
     }
 
-    private void HeroFlip()
+    private void HeroFlip()                                                       // flip
     {
         float moveInput = Input.GetAxis("Horizontal");
         if (moveInput != 0)
